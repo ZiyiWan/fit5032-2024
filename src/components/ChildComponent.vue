82a3f6786f92d70cb7511043915b01682bab5bea
Zane Wan <zwan0238@gmail.com>
2024-08-01 21:46:21 +1000

    first commit of vue app

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<!-- 子组件：ChildComponent.vue -->
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  },
+  msg2: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <p>{{ msg2 }}</p>
+  </div>
+</template>
---
+++ src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component-name">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.component-name {
+  font-family: Arial, sans-serif;
+  color: blue;
+}
+</style>
